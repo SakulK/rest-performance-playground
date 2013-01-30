35d9090c2639c77bf4c775493980a3db46326af2
Łukasz Krenski <sakulk@gmail.com>
2013-01-30 20:35:12 +0100

    turbo-boost spray impl with Scala Futures

--- impl/spray/src/main/scala/Boot.scala
+++ impl/spray/src/main/scala/Boot.scala
@@ -1,15 +1,20 @@
 import spray.routing.SimpleRoutingApp
 import spray.json._
 import spray.httpx.SprayJsonSupport
+import akka.actor._
+import scala.concurrent.Future
 
 object Boot extends App with SimpleRoutingApp with DefaultJsonProtocol with SprayJsonSupport {
 	implicit val resultFormat = jsonFormat1(Result)
+	implicit val printer = CompactPrinter
 
 	startServer(interface = "0.0.0.0", port = 8080) {
 		path("power" / IntNumber / IntNumber) { (base, exponent) =>
 			get {
 				complete {
-					Result(Math.pow(base, exponent).toInt)
+					Future {
+						Result(Math.pow(base, exponent).toInt)
+					}
 				}
 			}
 		}
